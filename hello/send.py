d89a67e4960d2dbf76f518b561901edbb4caf9e3
Only One <onlyonejke@gmail.com>
2015-01-20 06:21:20 -0500

    Added tutorials

--- hello/send.py
+++ hello/send.py
@@ -8,10 +8,10 @@ logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.CRITICAL)
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+# channel.queue_declare(queue='hello1')
 
 channel.basic_publish(exchange='',
                       routing_key='hello',
-                      body='Hello World!')
+                      body='Hello World123!')
 print " [x] Sent 'Hello World!'"
 connection.close()
